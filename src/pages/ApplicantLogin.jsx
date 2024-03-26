a8865ef03d68a6183bb7836518c005943cac107a
LeemarPaner <leemarpaner123@gmail.com>
2024-03-26 15:45:15 +0800

    update applicantlogin

--- src/pages/ApplicantLogin.jsx
+++ src/pages/ApplicantLogin.jsx
@@ -18,37 +18,37 @@ function ApplicantLogin() {
             <h2 className="font-primary text-black text-3xl m-20">WELCOME APPLICANT!</h2>
             
             <label
-              for="Username"
-              class="relative block rounded-md border border-black shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600 mb-6">
-            <input
-              type="text"
-              id="Username"
-              class="peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 h-12 w-80"
-              placeholder="Username"/>
+              htmlFor="Username"
+              className="relative block rounded-md border border-black shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600 mb-6">
+              <input
+                type="text"
+                id="Username"
+                className="peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 h-12 w-80"
+                placeholder="Username"/>
 
-            <span
-              class="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs"
-            >
-              Username
-            </span>
-          </label>
+              <span
+                className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs"
+              >
+                Username
+              </span>
+            </label>
 
 
-          <label
-            for="Password"
-            class="relative block rounded-md border border-black shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600 mb-6">
-            
-            <input
-              type="text"
-              id="Password"
-              class="peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 h-12 w-80"
-              placeholder="Password"/>
-            
-            <span
-              class="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
-              Password
-            </span>
-          </label>
+            <label
+              htmlFor="Password"
+              className="relative block rounded-md border border-black shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600 mb-6">
+              
+              <input
+                type="text"
+                id="Password"
+                className="peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 h-12 w-80"
+                placeholder="Password"/>
+              
+              <span
+                className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
+                Password
+              </span>
+            </label>
 
             <button className="font-secondary text-black bg-transparent h-12 w-60 mt-16 btn btn-neutral">Login</button>
           </div>
