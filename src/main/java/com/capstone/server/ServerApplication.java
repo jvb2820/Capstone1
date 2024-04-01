9b711aada180de273f96bdffe3b689090cc8ff8e
Niño Bacaoco <Nino.bacaoco@gmail.com>
2024-04-02 02:19:09 +0800

    test run

--- src/main/java/com/capstone/server/ServerApplication.java
+++ src/main/java/com/capstone/server/ServerApplication.java
@@ -1,13 +1,17 @@
 package com.capstone.server;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+		log.info("Successfully Running...");
+		System.out.println("test run");
 	}
 
 }
